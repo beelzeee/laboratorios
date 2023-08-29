8dacb45ce509f3ea51089b94ecce94f273fb1d10
beelzeee <142556936+beelzeee@users.noreply.github.com>
2023-08-29 16:46:20 -0400

    laboratorio 4 bueno

--- ejercicio 4.c
+++ ejercicio 4.c
@@ -9,14 +9,11 @@ int resultado;
 printf("ingrese una fecha\n");
 scanf("%d",&tiempo);
 
-resultado = tiempo/4;
+resultado = tiempo % 4;
 
 if(resultado % 4 !=0){
 	printf("el año no es bisiesto");
 }
-//else{
-//	printf("el año no es bisiesto");
-//}
 
 else if(resultado%100 != 0){
 	printf("el año no es bisiesto");
